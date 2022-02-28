4297c8fa446a391bd3ae985c27482da72841e2bc
umidshah070 <umidjon.donaboyev@yandex.com>
2022-03-01 00:36:38 +0500

    add element in mani.css and main.js

--- js/main.js
+++ js/main.js
@@ -6,37 +6,58 @@ var elMan = document.querySelector(".item__span-man");
 var elVelo = document.querySelector(".item__span-velo");
 var elCar = document.querySelector(".item__span-car");
 var elAir = document.querySelector(".item__span-air");
+var elResult = document.querySelector(".site__result");
 
 
-elForm.addEventListener("submit", function(e){
-  e.preventDefault();
 
-  var elInputValue = elInput.value;
+elForm.addEventListener("submit", function(evt){
+  evt.preventDefault();
+
+  var elInputValue = Number(elInput.value.trim());
    var manSpeed = 3.6;
    var veloSpeed= 20.1;
    var carSpeed = 70;
    var airSpeed = 800;
 
+
+   if(elInputValue <= 0 || isNaN(elInputValue)){
+    elResult.textContent = "0 dan katta raqam kiriting";
+    elResult.classList.add("site__eror");
+    return
+
+   }else{
+    elResult.classList.remove("site__eror");
+   }
+
    function man(){
-     return elInputValue / manSpeed
+     var hour = Math.floor(elInputValue / manSpeed);
+     var minut = Math.floor((elInputValue / manSpeed - hour) * 60);
+     return hour + " hour " + minut + " minut";
    }
 
    function velo(){
-    return elInputValue / veloSpeed
+    var hour = Math.floor(elInputValue / veloSpeed);
+    var minut = Math.floor((elInputValue / veloSpeed - hour) * 60);
+    return hour + " hour " + minut + " minut";
   }
 
   function car (){
-    return elInputValue / carSpeed
+    var hour = Math.floor(elInputValue / carSpeed);
+    var minut = Math.floor((elInputValue / carSpeed - hour) * 60);
+    return hour + " hour " + minut + " minut";
   }
 
   function air(){
-    return elInputValue / airSpeed
+    var hour = Math.floor(elInputValue / airSpeed);
+    var minut = Math.floor((elInputValue / airSpeed - hour) * 60);
+    return hour + " hour " + minut + " minut";
+
   }
 
-  elMan.textContent = Math.floor(man());
-  elVelo.textContent = Math.floor(velo());
-  elCar.textContent = Math.floor(car());
-  elAir.textContent = Math.floor(air());
+  elMan.textContent = man();
+  elVelo.textContent = velo();
+  elCar.textContent = car();
+  elAir.textContent = air();
 
 
 });
